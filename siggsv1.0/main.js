718bbe997299e329536f3b07ab60789f798c80da
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-08-18 04:24:38 +0200

    siggs initial commit

---
+++ siggsv1.0/main.js
@@ -0,0 +1,12 @@
+//zesa token 3325 6145 7821 3221 7909
+
+
+var current = new Date();
+current = document.getElementById(currentYear.getFullYear());
+
+//operating hrs
+var openHrs = 0800,closeHrs = 1700;
+
+var currentHrs = new Date();
+currentHrs =
+
