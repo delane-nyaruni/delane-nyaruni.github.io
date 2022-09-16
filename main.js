4130979db3ffa2db776dce66cf093aae897c72d7
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-09-17 00:41:34 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function helloVisitor() {
+    var morning = false;
+    if (morning) {
+        alert("Good Morning, visitor");
+    } else {
+        alert("Hello, visitor");
+    }
+}
+
+function byeVisitor() {
+    alert("Goodbye...");
+}
