998067266d70bac5c038a167c87c9bd894112294
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-09-19 23:06:11 +0200

    splendid academy website initial upload

---
+++ splendid academy/main.js
@@ -0,0 +1,12 @@
+function helloVisitor() {
+    var morning = false;
+    if (morning) {
+        alert("Good Morning, visitor");
+    } else {
+        alert("Hello, Welcome to Splendid Academy website");
+    }
+}
+
+function byeVisitor() {
+    alert("Goodbye...");
+}
