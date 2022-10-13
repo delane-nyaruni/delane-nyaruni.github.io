fbb0ace6d13fcca12a9f9f91198c1d8543c0f914
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-10-14 00:17:46 +0200

    dairy upload

---
+++ diary/index.js
@@ -0,0 +1,27 @@
+function validate() {
+    let userName = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+
+    if (userName === "delane" && password === "1234") {
+        alert("login success");
+        //login page redirect
+        newPage();
+    } else {
+        alert("login failed");
+    }
+
+}
+
+
+function newPage() {
+    location.href = 'diary.html';
+}
+
+function signout() {
+    alert("Signing Out...");
+    location.href = 'index.html';
+    //TODO: create a timeObject to use given different msgs from an array 
+    //randomly,can choose by date say friday have a great weknd, monday have a great week,
+    //wednesday how is your wk going
+    //show time
+}
