8ecf212b9ae9498099c26a40489ac36b901c36ec
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-10-16 01:09:19 +0200

    studentui overflow hidden

---
+++ studentui/student/student portal/registered subjects/registered_subjects.js
@@ -0,0 +1,19 @@
+function english() {
+    location.href = '';
+}
+
+function commerceSubject() {
+    location.href = '';
+}
+
+function computersSubject() {
+    location.href = './computers/index.html';
+}
+
+function geographySubject() {
+    location.href = '';
+}
+
+function scienceSubject() {
+    location.href = '';
+}
---
+++ studentui/student/student portal/student_portal.js
@@ -0,0 +1,7 @@
+function mailBox() {
+    location.href = 'mail/index.html';
+}
+
+function registeredSubjects() {
+    location.href = "registered subjects/index.html";
+}
