dd20dc52c0182ed9d67a9dcc16fbdd1e6087ecd4
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-10-15 02:10:27 +0200

    Delete student directory

--- student/student portal/student_portal.js
+++
@@ -1,7 +0,0 @@
-function mailBox() {
-    location.href = '/student/student portal/mail/index.html';
-}
-
-function registeredSubjects() {
-    location.href = "/student/student portal/registered subjects/index.html";
-}
